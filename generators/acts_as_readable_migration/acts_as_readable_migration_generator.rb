6128e06811cb4e9dc9e94f3462935a63f9efc22f
Thiago Jackiw <tjackiw@gmail.com>
2011-07-25 15:21:41 -0700

    Updating the plugin's generators to work with Rails 3

--- generators/acts_as_readable_migration/acts_as_readable_migration_generator.rb
+++ generators/acts_as_readable_migration/acts_as_readable_migration_generator.rb
@@ -1,11 +1,13 @@
-class ActsAsReadableMigrationGenerator < Rails::Generator::Base 
-  def manifest 
-    record do |m| 
-      m.migration_template 'migration.rb', 'db/migrate' 
-    end 
-  end
+class ActsAsReadableMigrationGenerator < Rails::Generators::Base
+  source_root File.expand_path('../templates', __FILE__)
+  argument :migration_name, :type => :string, :default => "acts_as_readable_migration"
   
+  def generate_acts_as_readable_migration
+    copy_file "migration.rb", "db/migrate/#{file_name}.rb"
+  end
+    
+  private
   def file_name
-    "acts_as_readable_migration"
+    Time.now.strftime("%Y%m%d%H%M%S") + '_' + migration_name.underscore
   end
 end
